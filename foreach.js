58e5665aebaf445512fd0ef4ef1c1de19b38665c
jasonrhoads <jasrhoads@gmail.com>
2020-07-04 14:18:15 +0000

    Add a rudimentary palindrome detector

---
+++ foreach.js
@@ -0,0 +1,25 @@
+let a =["ant", "bat", "cat", 42];
+
+a.forEach(function(element) {
+  console.log(element);
+});
+
+a.forEach((element) => { console.log(element) });
+
+let soliloquy = "To be, or not to be, that is the question:";
+
+Array.from(soliloquy).forEach(function(character) {
+  console.log(character);
+});
+
+a = [8, 17, 42, 99];
+console.log(a);
+a.sort();
+console.log(a);
+a.sort(function(a, b) { return a - b });
+console.log(a);
+a.sort();
+console.log(a);
+a.sort((a, b) => { return a - b });
+console.log(a);
+a.forEach(num => console.log(num));
